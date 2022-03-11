f1f20f2b7285ec0fbe803297b13cf0fa95590e61
Super_Archi <artem.komyagin@mail.ru>
2022-03-11 17:01:51 +0300

    add hw

---
+++ func.py
@@ -0,0 +1,7 @@
+
+
+def sal():
+    return t * r + p
+t = int(input('time- '))
+r = int(input('rate- '))
+p = int(input('prize- '))
---
+++ hw3_3.py
@@ -0,0 +1,14 @@
+ 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента
+# и возвращает сумму наибольших двух аргументов.
+
+
+def my_func():
+    print(sum)
+numbers=[]
+for i in range(3):
+    number=  int(input('number-'))
+    numbers.append(number)
+min=min(numbers)
+numbers.remove(min)
+sum=sum(numbers)
+my_func()
